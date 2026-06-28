851d3405176845c50de5f0ffa8a09daa45d8c9dc
Eva Raimundez <nepeta@gmail.com>
2026-06-28 16:36:21 +0100

    added email and password validators

---
+++ services/user/internal/core/validator/email.go
@@ -0,0 +1,9 @@
+package validator
+
+import "regexp"
+
+var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+
+func IsValidEmail(email string) bool {
+	return emailRegex.MatchString(email)
+}
