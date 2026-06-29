19a7ba8d69001f051500515bb1d7fef3cd9e8e99
Eva Raimundez <nepeta@gmail.com>
2026-06-29 11:21:55 +0100

    edited user config and docker compose to include missing services

--- services/user/internal/config/config.go
+++ services/user/internal/config/config.go
@@ -2,20 +2,26 @@ package config
 
 import (
 	"github.com/nepeta70/ride-hailing/internal/pkg/adapters/pgstore"
+	"github.com/nepeta70/ride-hailing/internal/pkg/adapters/pubsub"
+	"github.com/nepeta70/ride-hailing/internal/pkg/adapters/rdstore"
 	"github.com/nepeta70/ride-hailing/internal/pkg/config"
 )
 
 type Config struct {
 	config.BaseConfig
+	Redis    rdstore.RedisConfig    `json:"redis"`
 	Postgres pgstore.PostgresConfig `json:"postgres"`
+	Kafka    *pubsub.KafkaConfig    `json:"kafka"`
 }
 
 func DefaultConfig() *Config {
 	base := config.DefaultBaseConfig()
-	base.ServiceName = "User Service"
+	base.ServiceName = "user"
 	return &Config{
 		BaseConfig: base,
+		Redis:      rdstore.DefaultRedisConfig(),
 		Postgres:   pgstore.DefaultPostgresConfig(),
+		Kafka:      pubsub.DefaultKafkaConfig(),
 	}
 }
 
